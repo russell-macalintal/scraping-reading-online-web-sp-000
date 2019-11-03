d2f5c6d2dee5a8e9a657bd4eec1acd1cecb1c768
Russell Macalintal <russell.macalintal@gmail.com>
2019-11-03 04:14:53 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+list = doc.css('#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .title-oE5vT4')
+list.each {|elem| puts elem.text}
