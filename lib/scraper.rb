5aaddacaaab5c9aaf13d9b524a9005c9b3f5f698
Russell Macalintal <russell.macalintal@gmail.com>
2019-11-03 03:54:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,5 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
-doc.css('#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .title-oE5vT4').each {|elem| puts elem.text}
+list = doc.css('#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .title-oE5vT4')
+list.each {|elem| puts elem.text}
