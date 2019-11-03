31cc766c3507a0e74b3aecf408a6ec674463c9cb
Russell Macalintal <russell.macalintal@gmail.com>
2019-11-03 03:51:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,6 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+doc.css('.inner-3LPBeK inner-3LPBeK.title-oE5vT4').each {|elem| puts elem.text}
